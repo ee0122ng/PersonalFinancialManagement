16ecdf9a9f414ddc8c04e96decb6bd1e11f54550
ee0122ng <ee22011993hang@gmail.com>
2023-05-29 10:08:47 +0800

    initial server

---
+++ pfm_expenses_server/src/main/java/iss/ibf/pfm_expenses_server/PfmExpensesServerApplication.java
@@ -0,0 +1,13 @@
+package iss.ibf.pfm_expenses_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmExpensesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmExpensesServerApplication.class, args);
+	}
+
+}
